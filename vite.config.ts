a285fb1f2813eea01d90eaff7aff760b8ce347d8
denosaur98 <denischurchill1998@gmail.com>
2025-07-07 08:51:40 +0300

    fix script

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
+        script: resolve(__dirname, 'src/script.js')
       },
     },
   },
